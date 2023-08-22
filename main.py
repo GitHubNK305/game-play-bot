bf841d6788005d12fee8ddcd1b203565fc0559f8
Jin Tao <jin.tao@silo.ai>
2023-08-22 13:10:11 +0300

    update the main.py

--- main.py
+++ main.py
@@ -1,55 +1,56 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
-# chrome_driver_path = "C:/Users/35850/Downloads/chromedriver-win64/chromedriver-win64.exe"
-# service = Service(executable_path=chrome_driver_path)
-# options = webdriver.ChromeOptions()
-# driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://www.python.org/")
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
-# for event_time in event_times:
-#     print(event_time.get_attribute("datetime").split("T")[0])
+# Get cookies to click on
+cookie = driver.find_element(By.ID, "cookie")
 
-event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
-# for event in event_names:
-#     print(event.text)
+# Get price dic of afforded items
+price_names = []
+price_values = []
+price_dic = {}
 
-events = {}
-for n in range(len(event_times)):
-    events[n] = {
-        "name": event_times[n].get_attribute("datetime").split("T")[0],
-        "event": event_names[n].text
-    }
+prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
 
-print(events)
+for price in prices[:-1]:
+    price_names.append(price.text.split("-")[0].strip())
+    price_values.append(int(price.text.split("-")[1].strip().replace(",", "")))
 
-# event_names = driver.find_elements(By.CSS_SELECTOR,
-#                               "#content > div > section > div.list-widgets.row > div.medium-widget.event-widget.last > div > ul > li")
-# events_list = []
-# events_dic_list = []
-# events_dic = {}
-# for event in event_names:
-#     # print(event.text)
-#     # print(event.text.split("/n")[1])
-#     # dict = {event.text.split("/n")[0] : event.text.split("/n")[0]}
-#     events_list.append(event.text)
-#
-# for event in events_list:
-#     new_dic = {event.split("\n")[0]: event.split("\n")[1]}
-#     events_dic_list.append(new_dic)
-# # print(events_dic_list)
-#
-# keys = list(range(len(events_dic_list)))
-# # print(keys)
-# # for event in events_list:
-# #     events_dic
-# events_dic = {key: value for (key, value) in zip(keys, events_dic_list)}
-# print(events_dic)
+for n in range(len(price_names)):
+    price_dic[price_names[n]] = price_values[n]
 
-driver.quit()
+timeout = time.time() + 5  # 5 seconds to check items to buy
+time_min = time.time() + 60 * 5  # 5 mins break
+
+while True:
+    cookie.click()
+
+    # Every 5 seconds to check
+    if time.time() > timeout:
+        total_money = driver.find_element(By.ID, "money").text
+        if "," in total_money:
+            total_money = total_money.replace(",", "")
+
+        items = []
+        for price in price_values:
+            if int(total_money) >= price:
+                items.append(price_names[price_values.index(price)])
+        # print(f"You can buy {items}")
+        if len(items) >= 1:
+            buy_item = driver.find_element(By.ID, f"buy{items[-1]}")
+            buy_item.click()
+            print(f"You have bought {items[-1]}")
+
+        timeout = time.time() + 5
+
+    if time.time() > time_min:
+        cookie_per_second = driver.find_element(By.ID, "cps")
+        print(cookie_per_second.text)
+
+        break
